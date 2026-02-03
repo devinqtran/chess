60bca52687ded4a0b5e6a348b344bbe8cde33436
devinqtran <devinqtran@gmail.com>
2026-02-03 10:10:14 -0700

    Created AuthDAO Data Access Object interface for authentication.

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,10 +1,43 @@
 package dataaccess;
 
+
 import model.AuthData;
+
 
 /**
  * Data Access Object interface for Authentication operations
  */
 public interface AuthDAO {
 
+
+    /**
+     * Create a new authentication token
+     * @param auth The auth data to insert
+     * @throws DataAccessException if insertion fails
+     */
+    void insertAuth(AuthData auth) throws DataAccessException;
+
+
+    /**
+     * Retrieve authentication data by token
+     * @param authToken The auth token to look up
+     * @return The AuthData object, or null if not found
+     * @throws DataAccessException if retrieval fails
+     */
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+
+    /**
+     * Delete an authentication token (logout)
+     * @param authToken The auth token to delete
+     * @throws DataAccessException if the token doesn't exist or deletion fails
+     */
+    void deleteAuth(String authToken) throws DataAccessException;
+
+
+    /**
+     * Clear all authentication tokens from the database
+     * @throws DataAccessException if clearing fails
+     */
+    void clear() throws DataAccessException;
 }
