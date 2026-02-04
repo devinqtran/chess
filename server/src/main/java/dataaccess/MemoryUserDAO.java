f8d712c91d4f9e861dd3757239313d24af53a61f
devinqtran <devinqtran@gmail.com>
2026-02-04 08:35:38 -0700

    Added UserDAO and MemoryUserDAO objects

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,12 +1,43 @@
 package dataaccess;
 
+
 import model.UserData;
 import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * In-memory implementation of UserDAO using HashMap
  */
 public class MemoryUserDAO implements UserDAO {
 
+
+    private final Map<String, UserData> users = new HashMap<>();
+
+
+    @Override
+    public void insertUser(UserData user) throws DataAccessException {
+        if (user == null || user.username() == null) {
+            throw new DataAccessException("User or username cannot be null");
+        }
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        if (username == null) {
+            throw new DataAccessException("Username cannot be null");
+        }
+        return users.get(username);
+    }
+
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+    }
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,10 +1,35 @@
 package dataaccess;
 
+
 import model.UserData;
+
 
 /**
  * Data Access Object interface for User operations
  */
 public interface UserDAO {
 
+
+    /**
+     * Create a new user in the database
+     * @param user The user data to insert
+     * @throws DataAccessException if the user already exists or insertion fails
+     */
+    void insertUser(UserData user) throws DataAccessException;
+
+
+    /**
+     * Retrieve a user by username
+     * @param username The username to look up
+     * @return The UserData object, or null if not found
+     * @throws DataAccessException if retrieval fails
+     */
+    UserData getUser(String username) throws DataAccessException;
+
+
+    /**
+     * Clear all users from the database
+     * @throws DataAccessException if clearing fails
+     */
+    void clear() throws DataAccessException;
 }
