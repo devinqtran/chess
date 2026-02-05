9b2420a43a16e6f49e11d7a176c75f8c68fc9e53
devinqtran <devinqtran@gmail.com>
2026-02-05 07:46:57 -0700

    Finished MemoryUserDAO and MemoryGameDAO passes current tests.

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,12 +1,46 @@
 package dataaccess;
 
+
 import model.AuthData;
 import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * In-memory implementation of AuthDAO using HashMap
  */
 public class MemoryAuthDAO implements AuthDAO {
+    private final Map<String, AuthData> authTokens = new HashMap<>();
 
+    @Override
+    public void insertAuth(AuthData auth) throws DataAccessException {
+        if (auth == null || auth.authToken() == null) {
+            throw new DataAccessException("Auth or authToken cannot be null");
+        }
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        if (authToken == null) {
+            throw new DataAccessException("AuthToken cannot be null");
+        }
+        return authTokens.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        if (authToken == null) {
+            throw new DataAccessException("AuthToken cannot be null");
+        }
+        if (!authTokens.containsKey(authToken)) {
+            throw new DataAccessException("AuthToken not found");
+        }
+        authTokens.remove(authToken);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        authTokens.clear();
+    }
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,13 +1,55 @@
 package dataaccess;
 
+
 import model.GameData;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+
 /**
  * In-memory implementation of GameDAO using HashMap
  */
 public class MemoryGameDAO implements GameDAO {
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private int nextGameID = 1;
 
+    @Override
+    public int insertGame(GameData game) throws DataAccessException {
+        if (game == null) {
+            throw new DataAccessException("Game cannot be null");
+        }
+        int gameID = nextGameID++;
+        GameData newGame = new GameData(gameID, game.whiteUsername(),
+                game.blackUsername(), game.gameName(), game.game());
+        games.put(gameID, newGame);
+        return gameID;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return games.get(gameID);
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return games.values();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        if (game == null) {
+            throw new DataAccessException("Game cannot be null");
+        }
+        if (!games.containsKey(game.gameID())) {
+            throw new DataAccessException("Game not found");
+        }
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        games.clear();
+        nextGameID = 1;
+    }
 }
