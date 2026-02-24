cac708b976242982e02ce13837ea5808f38db036
devinqtran <devinqtran@gmail.com>
2026-02-24 07:34:01 -0700

    Created MySqlAuthDAO class

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -1,4 +1,67 @@
 package dataaccess;
 
-public class MySqlAuthDAO {
+import model.AuthData;
+import java.sql.SQLException;
+
+/**
+ * MySQL version of AuthDAO interface
+ */
+public class MySqlAuthDAO implements AuthDAO {
+
+    @Override
+    public void insertAuth(AuthData auth) throws DataAccessException {
+        var sql = "INSERT INTO authTokens (authToken, username) VALUES (?, ?)";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, auth.authToken());
+            ps.setString(2, auth.username());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        var sql = "SELECT authToken, username FROM authTokens WHERE authToken = ?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, authToken);
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new AuthData(rs.getString("authToken"), rs.getString("username"));
+                }
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        var sql = "DELETE FROM authTokens WHERE authToken = ?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, authToken);
+            int rows = ps.executeUpdate();
+            if (rows == 0) {
+                throw new DataAccessException("Error: unauthorized");
+            }
+        } catch (DataAccessException e) {
+            throw e;
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement("TRUNCATE TABLE authTokens")) {
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: " + e.getMessage());
+        }
+    }
 }
