622146b75236ebebf644580434e10acb0d45d686
devinqtran <devinqtran@gmail.com>
2026-03-10 11:08:10 -0600

    Implemented Repl.java and created State and BoardRenderer classes

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -1,4 +1,35 @@
 package client;
 
+import java.util.Scanner;
+
 public class Repl {
+    private final ServerFacade facade;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public Repl(int port) {
+        this.facade = new ServerFacade(port);
+    }
+
+    public void run() {
+        System.out.println("♕ Welcome to 240 Chess! Type 'help' to get started. ♕");
+
+        PreloginClient preloginClient = new PreloginClient(facade, scanner);
+
+        // Start in prelogin state loop until "quit"
+        State state = State.PRELOGIN;
+        while (state != State.QUIT) {
+            switch (state) {
+                case PRELOGIN -> state = preloginClient.run();
+                case POSTLOGIN -> {
+                    PostloginClient postloginClient = new PostloginClient(facade, scanner, preloginClient.getAuthToken());
+                    // Keep looping inside postlogin until logout or quit
+                    while (state == State.POSTLOGIN) {
+                        state = postloginClient.run();
+                    }
+                    preloginClient.clearAuthToken();
+                }
+            }
+        }
+        System.out.println("Goodbye!");
+    }
 }
