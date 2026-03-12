5f8eeaa00a69a72f0a11a619f2b330f51fd0c106
devinqtran <devinqtran@gmail.com>
2026-03-12 08:42:14 -0600

    Created needed methods and implemented ServerMain, code compiles correctly

--- client/src/main/java/client/PostloginClient.java
+++ client/src/main/java/client/PostloginClient.java
@@ -1,4 +1,23 @@
 package client;
 
+import java.util.Scanner;
+
 public class PostloginClient {
+    private final ServerFacade facade;
+    private final Scanner scanner;
+    private final String authToken;
+
+    public PostloginClient(ServerFacade facade, Scanner scanner, String authToken) {
+        this.facade = facade;
+        this.scanner = scanner;
+        this.authToken = authToken;
+    }
+
+    // Temporary stub
+    public State run() {
+        System.out.print("\n[LOGGED IN] >>> ");
+        scanner.nextLine();
+        return State.PRELOGIN;
+    }
 }
+
--- client/src/main/java/client/PreloginClient.java
+++ client/src/main/java/client/PreloginClient.java
@@ -1,4 +1,24 @@
 package client;
 
+import java.util.Scanner;
+
 public class PreloginClient {
+    private final ServerFacade facade;
+    private final Scanner scanner;
+    private String authToken = null;
+
+    public PreloginClient(ServerFacade facade, Scanner scanner) {
+        this.facade = facade;
+        this.scanner = scanner;
+    }
+
+    // Temporary stub
+    public State run() {
+        System.out.print("\n[LOGGED OUT] >>> ");
+        scanner.nextLine();
+        return State.QUIT;
+    }
+
+    public String getAuthToken() { return authToken; }
+    public void clearAuthToken() { authToken = null; }
 }
