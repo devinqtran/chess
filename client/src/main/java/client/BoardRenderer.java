2f7ddfa241fd237c7b638476b085dd2bc930d6d5
devinqtran <devinqtran@gmail.com>
2026-03-13 09:02:59 -0600

    Switched empty space to 3-space string instead of EscapeSequences.EMPTY, board now formats correctly

--- client/src/main/java/client/BoardRenderer.java
+++ client/src/main/java/client/BoardRenderer.java
@@ -22,24 +22,24 @@ public class BoardRenderer {
             int chessRow = whitePerspective ? (8 - row) : (row + 1);
 
             // Left border
-            System.out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY +
-                    EscapeSequences.SET_TEXT_COLOR_WHITE +
+            System.out.print(EscapeSequences.SET_BG_COLOR_BLACK+
+                    EscapeSequences.SET_TEXT_COLOR_WHITE + EscapeSequences.SET_TEXT_BOLD +
                     " " + chessRow + " " +
                     EscapeSequences.RESET_BG_COLOR +
                     EscapeSequences.RESET_TEXT_COLOR);
 
-            // Empty squares for now
+            // Empty squares
             for (int col = 0; col < 8; col++) {
                 boolean isLightSquare = (chessRow + (whitePerspective ? col + 1 : 8 - col)) % 2 == 0;
                 String bg = isLightSquare
                         ? EscapeSequences.SET_BG_COLOR_LIGHT_GREY
                         : EscapeSequences.SET_BG_COLOR_BLUE;
-                System.out.print(bg + EscapeSequences.EMPTY + EscapeSequences.RESET_BG_COLOR);
+                System.out.print(bg + "   " + EscapeSequences.RESET_BG_COLOR);
             }
 
             // Right border
-            System.out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY +
-                    EscapeSequences.SET_TEXT_COLOR_WHITE +
+            System.out.print(EscapeSequences.SET_BG_COLOR_BLACK +
+                    EscapeSequences.SET_TEXT_COLOR_WHITE + EscapeSequences.SET_TEXT_BOLD +
                     " " + chessRow + " " +
                     EscapeSequences.RESET_BG_COLOR +
                     EscapeSequences.RESET_TEXT_COLOR);
@@ -52,7 +52,7 @@ public class BoardRenderer {
 
     // Method for printing the column labels based on team color
     private static void printColumnLabels(boolean whitePerspective) {
-        System.out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY + EscapeSequences.SET_TEXT_BOLD + EscapeSequences.SET_TEXT_COLOR_WHITE + "   ");
+        System.out.print(EscapeSequences.SET_BG_COLOR_BLACK + EscapeSequences.SET_TEXT_BOLD + EscapeSequences.SET_TEXT_COLOR_WHITE + "   ");
         for (int col = 0; col < 8; col++) {
             int chessColumn = whitePerspective ? (col + 1) : (8 - col);
             System.out.print(" " + COLUMN_LABELS[chessColumn - 1] + " ");
