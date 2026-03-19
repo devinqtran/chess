46b487b46a064d1a4abbbbefcb3332ac35bb3832
devinqtran <devinqtran@gmail.com>
2026-03-19 09:52:17 -0600

    Implemented onMessage to handle receiving of messages, builds and passes all 'connect' tests

--- server/src/main/java/server/handlers/WebSocketHandler.java
+++ server/src/main/java/server/handlers/WebSocketHandler.java
@@ -1,7 +1,14 @@
 package server;
 
+import model.AuthData;
+import model.GameData;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
 import com.google.gson.Gson;
 import dataaccess.*;
+import java.io.IOException;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
 
@@ -33,7 +40,65 @@ public class WebSocketHandler {
     }
 
     @OnWebSocketMessage
-    public void onMessage(Session session, String message) {
+    public void onMessage(Session session, String message) throws IOException {
         System.out.println("WebSocket message received: " + message);
+        UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
+
+        try {
+            // Validate the auth token
+            AuthData auth = authDAO.getAuth(command.getAuthToken());
+            if (auth == null) {
+                sendError(session, "Error: invalid auth token");
+                return;
+            }
+
+            // Route to handler
+            switch (command.getCommandType()) {
+                case CONNECT -> handleConnect(session, command, auth.username());
+                case MAKE_MOVE -> System.out.println("Make move coming soon");
+                case LEAVE -> System.out.println("Leave coming soon");
+                case RESIGN -> System.out.println("Resign coming soon");
+            }
+        } catch (DataAccessException e) {
+            sendError(session, "Error: " + e.getMessage());
+        }
+    }
+
+    // Helper methods
+    private void handleConnect(Session session, UserGameCommand command,
+                               String username) throws IOException, DataAccessException {
+        int gameID = command.getGameID();
+        GameData gameData = gameDAO.getGame(gameID);
+
+        if (gameData == null) {
+            sendError(session, "Error: game not found");
+            return;
+        }
+
+        // Add session to the game
+        sessions.addSession(gameID, session);
+
+        // Send LOAD_GAME back to the connecting client
+        var loadGame = new LoadGameMessage(gameData.game());
+        sessions.sendToSession(session, gson.toJson(loadGame));
+
+        // Build notification message
+        String notification;
+        if (username.equals(gameData.whiteUsername())) {
+            notification = username + " joined the game as WHITE";
+        } else if (username.equals(gameData.blackUsername())) {
+            notification = username + " joined the game as BLACK";
+        } else {
+            notification = username + " is observing the game";
+        }
+
+        // Send to all other clients
+        var notificationMessage = new NotificationMessage(notification);
+        sessions.broadcast(gameID, gson.toJson(notificationMessage), session);
+    }
+
+    private void sendError(Session session, String message) throws IOException {
+        var error = new ErrorMessage(message);
+        sessions.sendToSession(session, gson.toJson(error));
     }
 }
